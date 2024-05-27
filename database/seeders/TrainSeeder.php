176df5fcf452b8b7483a6b8a4584044c43318f1b
orlando manfredini <orlando.manfre1@gmail.com>
2024-05-27 15:43:16 +0200

    ADD: faker TrainSeeder

--- database/seeders/TrainSeeder.php
+++ database/seeders/TrainSeeder.php
@@ -5,8 +5,10 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use Faker\Generator as Faker;
 use App\Models\Train;
+use Faker\Generator as Faker;
+use Illuminate\Support\Facades\DB;
+
 
 class TrainSeeder extends Seeder
 {
@@ -17,16 +19,20 @@ class TrainSeeder extends Seeder
     {
         //
         for ($i = 0; $i < 50; $i++) {
-            $new_train = new Train;
+            $new_train = new Train();
             $new_train->company = $faker->company();
             $new_train->departure = $faker->city();
             $new_train->arrival = $faker->city();
             $new_train->departure_time = $faker->time();
             $new_train->arrival_time= $faker->time();
-            $new_train->arrival_time= $faker->bothify('??-#####');
+            $new_train->code_transport= $faker->unique()->bothify('??-#####');
             $new_train->carriage= $faker->randomNumber(2);
             $new_train->margin_time= $faker->randomNumber(2);
             $new_train->cancelled= $faker->boolean();
+
+            dump($new_train);
+
+            $new_train->save();
 
 
 
